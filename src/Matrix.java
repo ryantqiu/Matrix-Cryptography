2d13d938341bd44e1f52ae6df245b0b6549dd2c7
Ryan Qiu <ryantqiu@gmail.com>
2018-02-14 19:27:11 -0800

    Add files via upload

---
+++ src/Matrix.java
@@ -0,0 +1,17 @@
+
+public class Matrix {
+    private int[][] matrix;
+    private int n;
+    private int m;
+    private int[][] reducedEchelonForm;
+    
+    public Matrix(int[][] matrix) {
+        this.matrix = matrix;
+        this.n = matrix.length;
+        this.m = matrix[0].length;
+    }
+    
+    public Matrix reduce (Matrix matrix) {
+        
+    }
+}
