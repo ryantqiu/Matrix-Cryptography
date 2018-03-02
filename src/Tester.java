bfb0890a991ec94dd44947aa54ee23363a574206
rtq <rtq@uw.edu>
2018-03-02 14:37:16 -0800

    Deleted old matrix class

--- src/Tester.java
+++ src/Tester.java
@@ -1,34 +1,14 @@
-import java.util.Random;
+import Jama.*;
 
 public class Tester {
-    public static void main(String[] args) {
-        Matrix one = fillMatrix(new Matrix(new double[5][3]));
-        Matrix two = fillMatrix(new Matrix(new double[3][4]));
+    public static void main(String[] args) { 
+        Matrix test1 = new Matrix(new double[][]{{1,2,3},{4,7,9},{1,6,2}});
+        Matrix test1Inv = test1.inverse();
         
-        printMatrix(one);
-        printMatrix(two);
-        printMatrix(one.multiply(two));
-    }
-    
-    public static void printMatrix(Matrix mtx) {
-        double[][] matrix = mtx.getMatrix();
-        for (int i = 0; i < mtx.getN(); i++) {
-            System.out.print("|");
-            for (int j = 0; j < mtx.getM(); j++) {
-                System.out.print(" " + matrix[i][j]);
-            }
-            System.out.println(" |");
-        }
-    }
-    
-    public static Matrix fillMatrix(Matrix mtx) {
-        double[][] matrix = mtx.getMatrix();
-        Random random = new Random();
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                matrix[i][j] = random.nextInt(10);
-            }
-        }
-        return new Matrix(matrix);
+        test1.print(0, 0);
+        test1Inv.print(0, 0);
+        test1.print(0, 0);
+        Matrix id = test1.times(test1Inv);
+        test1.times(id).print(0, 0);
     }
 }
