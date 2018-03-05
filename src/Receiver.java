f4c1da77c88adb37c5342d5b862bb6a1fa868a51
wbigelow <wbigelow@uw.edu>
2018-03-04 17:00:22 -0800

    Testing for relative primes

--- src/Receiver.java
+++ src/Receiver.java
@@ -23,8 +23,11 @@ public class Receiver {
     
     public PublicKey createKeyPair() {
         Matrix a = generateRandomMatrix(rank);
+        while(gcd((int) a.det(), modulus) != 1) {
+            a = generateRandomMatrix(rank);
+        }
         privateKey = generateRandomMatrix(rank);
-        while (a.times(privateKey).getArray().equals(privateKey.times(a).getArray()) || (privateKey.times(a)).times(privateKey).det() == 0) { // makes sure ac != ca
+        while (a.times(privateKey).getArray().equals(privateKey.times(a).getArray()) || (privateKey.times(a)).times(privateKey).det() == 0 || gcd((int) privateKey.det(), modulus) != 1) { // makes sure ac != ca
             privateKey = generateRandomMatrix(rank);
         }
         Matrix b = Common.modMatrix(modulus, (privateKey.times(a)).times(privateKey)); // b = cac
@@ -60,6 +63,26 @@ public class Receiver {
             mtx[i][i] = 1 + random.nextInt(modulus - 1);
         }
         return new Matrix(mtx).transpose();
+    }
+    
+    public static int gcd(int p, int q) {
+        if (q == 0) return p;
+        if (p == 0) return q;
+
+        // p and q even
+        if ((p & 1) == 0 && (q & 1) == 0) return gcd(p >> 1, q >> 1) << 1;
+
+        // p is even, q is odd
+        else if ((p & 1) == 0) return gcd(p >> 1, q);
+
+        // p is odd, q is even
+        else if ((q & 1) == 0) return gcd(p, q >> 1);
+
+        // p and q odd, p >= q
+        else if (p >= q) return gcd((p-q) >> 1, q);
+
+        // p and q odd, p < q
+        else return gcd(p, (q-p) >> 1);
     }
     
     public int[] generatePrimes(int maxValue) {
